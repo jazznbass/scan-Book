2d1f88168cb25108b43a47bdef319cf5d72f32cb
Jürgen Wilbert <jazznbass98@gmail.com>
2019-08-02 15:22:35 +0200

    Google Analytics

---
+++ _common.R
@@ -0,0 +1,17 @@
+
+library(scan)
+library(knitr)
+library(kableExtra)
+library(pander)
+library(tidyverse)
+
+knitr::opts_chunk$set(echo = TRUE)
+#knitr::opts_chunk$set(tidy = TRUE)
+#knitr::opts_chunk$set(tidy.opts=list(width.cutoff=45))
+#options(knitr.table.format = "markdown")
+#options(width = 76) ##55 for A5
+
+# automatically create a bib database for R packages
+knitr::write_bib(c(
+  .packages(), 'bookdown', 'knitr', 'rmarkdown', 'scan'
+), 'packages.bib')
