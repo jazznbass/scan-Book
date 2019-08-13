8fb765c573eaa9d15d97d94bcb5f1d8ebe272c36
Jürgen Wilbert <jazznbass98@gmail.com>
2019-08-13 08:43:33 +0200

    print_table as a template for tables

--- _common.R
+++ _common.R
@@ -16,3 +16,14 @@ knitr::write_bib(c(
   .packages(), 'bookdown', 'knitr', 'rmarkdown', 'scan'
 ), 'packages.bib')
 
+print_table <- function(data, caption) {
+  
+  #panderOptions("table.split.cells", 60)
+  #set.alignment("left","left")
+  #pander(out, caption = CAPTION, justify = "left", keep.line.breaks = TRUE)
+  kable(data, caption = caption, booktabs = TRUE) %>%
+    kable_styling(full_width = FALSE) %>%
+    kable_styling(latex_options = c("repeat_header")) %>%
+    column_spec(1, bold = TRUE) %>%
+    column_spec(2, width = "30em")
+}
